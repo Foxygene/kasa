2ac735634373ed7180a911717d6822bb04101249
Foxygene <foxygene.dev@gmail.com>
2023-08-03 20:13:27 +0200

    feat(banner): added to flow

---
+++ src/components/banner.tsx
@@ -0,0 +1,13 @@
+import "./banner.css";
+
+function Banner() {
+  return (
+    <div className="banner">
+      <a>
+        Chez vous,<br></br> partout et ailleurs
+      </a>
+    </div>
+  );
+}
+
+export default Banner;
