72907891ff2300bf8896c2f34bfd4605b0495fbd
Foxygene <foxygene.dev@gmail.com>
2023-08-11 14:56:22 +0200

    chore(pages): added main

--- src/pages/ErrorPage.tsx
+++ src/pages/ErrorPage.tsx
@@ -1,6 +1,7 @@
 import { useRouteError } from "react-router-dom";
 import Header from "../components/Header";
 import "./errorPage.css";
+import Footer from "../components/Footer";
 
 export default function ErrorPage() {
   const error = useRouteError();
@@ -8,12 +9,15 @@ export default function ErrorPage() {
 
   return (
     <>
-      <Header />
-      <div className="error">
-        <p className="number">{error.status}</p>
-        <p>Oups! La page que vous demandez n'existe pas.</p>
-        <a href="/">Retourner sur la page d'acceuil</a>
-      </div>
+      <main>
+        <Header />
+        <div className="error">
+          <p className="number">{error.status}</p>
+          <p>Oups! La page que vous demandez n'existe pas.</p>
+          <a href="/">Retourner sur la page d'acceuil</a>
+        </div>
+      </main>
+      <Footer />
     </>
   );
 }
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,17 +2,21 @@ import Banner from "../components/Banner";
 import Card from "../components/Card";
 import Header from "../components/Header";
 import logements from "../assets/logements.json";
+import Footer from "../components/Footer";
 
 function HomePage() {
   return (
     <>
-      <Header />
-      <Banner />
-      <div className="list">
-        {logements.map((logement) => (
-          <Card key={logement.id} data={logement} />
-        ))}
-      </div>
+      <main>
+        <Header />
+        <Banner />
+        <div className="list">
+          {logements.map((logement) => (
+            <Card key={logement.id} data={logement} />
+          ))}
+        </div>
+      </main>
+      <Footer />
     </>
   );
 }
