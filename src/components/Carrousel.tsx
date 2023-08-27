3107807b80ded61a05edc12fdd193d70c078edeb
Foxygene <foxygene.dev@gmail.com>
2023-08-27 11:08:46 +0200

    feat(carousel): now fonctionnal with backgroundimage

--- src/components/Carrousel.tsx
+++ src/components/Carrousel.tsx
@@ -1,27 +1,35 @@
 import "./carrousel.css";
 import left from "../assets/left.svg";
 import right from "../assets/right.svg";
+import { useState } from "react";
 
 function Carrousel(props: { pictures: string[] }) {
+  const [slide, setSlide] = useState(0);
+
+  function handleClickRight() {
+    if (slide + 1 === props.pictures.length) return;
+
+    setSlide(slide + 1);
+  }
+
+  function handleClickLeft() {
+    if (slide - 1 === -1) return;
+    setSlide(slide - 1);
+  }
+
   return (
     <div className="carousel">
-      <div className="track-container">
-        <ul className="track">
-          {props.pictures.map((picture, i) => (
-            <li
-              style={{ left: `${1240 * i}px` }}
-              key={picture}
-              className="slide"
-            >
-              <img src={picture} />
-            </li>
-          ))}
-        </ul>
-      </div>
-      <button className="carousel-button button-left">
+      <div
+        className="track-container"
+        style={{ backgroundImage: `url(${props.pictures[slide]})` }}
+      ></div>
+      <button onClick={handleClickLeft} className="carousel-button button-left">
         <img src={left} />
       </button>
-      <button className="carousel-button button-right">
+      <button
+        onClick={handleClickRight}
+        className="carousel-button button-right"
+      >
         <img src={right} />
       </button>
     </div>
