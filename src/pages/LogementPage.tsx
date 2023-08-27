94c35f9c2f977ab758e2b5b3258ee4932dfa4356
Foxygene <foxygene.dev@gmail.com>
2023-08-27 11:48:46 +0200

    feat(logement page): added footer

--- src/pages/LogementPage.tsx
+++ src/pages/LogementPage.tsx
@@ -1,4 +1,5 @@
 import Carrousel from "../components/Carrousel";
+import Footer from "../components/Footer";
 import Header from "../components/Header";
 import { logement } from "../types/logement";
 import "./logementPage.css";
@@ -13,6 +14,7 @@ function Logement(prop: logement) {
         <Header />
         <Carrousel pictures={pictures} />
       </main>
+      <Footer />
     </>
   );
 }
