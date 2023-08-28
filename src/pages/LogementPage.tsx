605292275aeec0fc2609363352be54105064c87d
Foxygene <foxygene.dev@gmail.com>
2023-08-28 05:28:43 +0200

    feat(logement page): added logement info (WIP)

--- src/pages/LogementPage.tsx
+++ src/pages/LogementPage.tsx
@@ -1,6 +1,7 @@
 import Carrousel from "../components/Carrousel";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Stars from "../components/Stars";
 import { logement } from "../types/logement";
 import "./logementPage.css";
 
@@ -13,6 +14,21 @@ function Logement(prop: logement) {
       <main>
         <Header />
         <Carrousel pictures={pictures} />
+        <div>
+          <div>
+            <h1>{prop.data.title}</h1>
+            <p>{prop.data.location}</p>
+            {prop.data.tags.map((tag) => (
+              <li className="tag">{tag}</li>
+            ))}
+          </div>
+          <div>
+            <div>
+              <p>{prop.data.host.name}</p>
+              <img src={prop.data.host.picture} />
+            </div>
+          </div>
+        </div>
       </main>
       <Footer />
     </>
