ff61e223cca5c1bafc9de750bb803ba18a30f455
Foxygene <foxygene.dev@gmail.com>
2023-09-14 21:16:36 +0200

    fix(router): now uses HashRouter

--- src/route.tsx
+++ src/route.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createHashRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ErrorPage from "./pages/ErrorPage";
 import logements from "./assets/logements.json";
@@ -11,7 +11,7 @@ const logementPages = logements.map((logement) => ({
   errorElement: <ErrorPage />,
 }));
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: "/",
     element: <HomePage />,
