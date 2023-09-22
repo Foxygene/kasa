6d903913eaa70fa3d1ac064ef361e73e9fe29792
Foxygene <foxygene.dev@gmail.com>
2023-09-22 20:57:05 +0200

    fix(router): fixed typo

--- src/route.tsx
+++ src/route.tsx
@@ -2,12 +2,12 @@ import { createHashRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ErrorPage from "./pages/ErrorPage";
 import logements from "./assets/logements.json";
-import Logement from "./pages/LogementPage";
+import LogementPage from "./pages/LogementPage";
 import AboutPage from "./pages/AboutPage";
 
 const logementPages = logements.map((logement) => ({
   path: `/logement/${logement.id}`,
-  element: <Logement key={logement.id} data={logement} />,
+  element: <LogementPage key={logement.id} data={logement} />,
   errorElement: <ErrorPage />,
 }));
 
